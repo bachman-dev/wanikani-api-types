433a3014da8c8c2acceaccfc031e10e6bde499e8
Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>
2025-03-21 18:28:58 -0400

    Added messages for Review Payloads
    Signed-off-by: Collin Bachman <3395010+bachmacintosh@users.noreply.github.com>

--- src/v20170710/lang/index.ts
+++ src/v20170710/lang/index.ts
@@ -1,2 +1,4 @@
 export * from "./api-revision.js";
 export * from "./date-union.js";
+export * from "./review-payload-intersect.js";
+export * from "./review-payload-union.js";
---
+++ src/v20170710/lang/review-payload-intersect.ts
@@ -0,0 +1,15 @@
+import type * as v from "valibot";
+import { getLocale } from "./_internal.js";
+
+type Message = v.ErrorMessage<v.IntersectIssue>;
+
+const en: Message =
+  "Failed to intersect Review Payload objects; this is usually caused when trying to merge a base object with key/value type conflicts.";
+
+export const reviewPayloadIntersect: Message = (issue) => {
+  const locale = getLocale(issue);
+  switch (locale) {
+    default:
+      return en;
+  }
+};
